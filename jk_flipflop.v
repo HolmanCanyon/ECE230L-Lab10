6714e129df9d6efac268c560ff8f728b833465f7
HolmanCanyon <canyonholman@u.boisestate.edu>
2026-03-31 16:18:38 +0000

    a

--- jk_flipflop.v
+++ jk_flipflop.v
@@ -1,6 +1,7 @@
 module jk_flipflop (
   input J, K, Clock
   output reg Q
+  output NotQ
 );
 
   initial begin 
@@ -8,12 +9,16 @@ module jk_flipflop (
   end
   
   
-  always @(posedge Clock)begin 
+  always @(posedge Clock) begin 
     if (Q == 0) begin
       Q <= J
     end
+    
     else begin
       Q <= ~K
     end
   end
+
+  assign NotQ = ~Q;
+
   endmodule 
