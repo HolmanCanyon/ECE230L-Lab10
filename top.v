140b605c39ea555c07411769360f6ad28ae23edb
HolmanCanyon <canyonholman@u.boisestate.edu>
2026-03-31 16:11:05 +0000

    JKFlipFlop added to top.v

--- top.v
+++ top.v
@@ -17,4 +17,12 @@ module top(
         .Clock(btnC)
     );
 
+    JKFlipFlop JKFlipFlop(
+        .J(sw[1]),
+        .K(sw[2])
+        .Q(led[2]),
+        .NotQ(led[3]),
+        .Clock(btnC)
+    );
+
 endmodule
