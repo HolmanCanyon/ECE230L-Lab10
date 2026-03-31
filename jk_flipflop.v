75608f5caa8f790e930f272c8a7eda2acbe4d550
codyt-bsu <codytoone@u.boisestate.edu>
2026-03-31 10:14:57 -0600

    finish? JK flip-flop module

--- jk_flipflop.v
+++ jk_flipflop.v
@@ -1,12 +1,19 @@
 module jk_flipflop (
-  input J, K,
+  input J, K, Clock
   output reg Q
 );
 
   initial begin 
     Q <= 0
   end
-
-  always @(posedge J, K)
-    
+  
+  
+  always @(posedge Clock)begin 
+    if (Q == 0) begin
+      Q <= J
+    end
+    else begin
+      Q <= ~K
+    end
+  end
   endmodule 
