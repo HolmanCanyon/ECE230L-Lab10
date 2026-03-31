759c682a40f80ca6b6ab0032cc3cf4eb3ba8e394
HolmanCanyon <canyonholman@u.boisestate.edu>
2026-03-31 16:26:41 +0000

    a

---
+++ JKflipflop.v
@@ -0,0 +1,27 @@
+module JKFlipFlop (
+  input J, K, Clock,
+  output reg Q,
+  output NotQ
+);
+
+  initial begin 
+    Q <= 0
+  end
+  
+  
+  always @(posedge Clock) begin 
+    if (J & K) begin 
+      Q <= ~Q
+    end else begin
+      if (Q) begin
+        Q <= ~K
+      end
+      else begin
+        Q <= J
+      end
+    end
+  end
+
+  assign NotQ = ~Q;
+
+  endmodule 
